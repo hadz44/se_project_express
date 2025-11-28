3820c0c9834eec9a47561125d065a5679743204c
Hadi Frifer <doshka44@gmail.com>
2025-11-28 13:16:38 -0500

    alterations

--- restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# PM2 Restart Script for WTWR Backend
-# Usage: ./restart.sh
-
-cd /home/doshka44_gmail_com/apps/items   # or wherever your backend lives
-
-pm2 restart all   # or pm2 restart <your-app-name>
-
